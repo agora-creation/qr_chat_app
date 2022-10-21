540a700eeee3c69f4340cdbfcbea8435314d7e5a
agora.creation <apple@agora-c.com>
2022-10-21 16:06:35 +0900

    20221021 commit

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_chat_app/widgets/custom_text_form_field.dart';
+import 'package:qr_chat_app/widgets/round_lg_button.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({Key? key}) : super(key: key);
@@ -15,6 +17,38 @@ class UserScreen extends StatelessWidget {
           IconButton(
             onPressed: () => Navigator.of(context, rootNavigator: true).pop(),
             icon: const Icon(Icons.close),
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+        children: [
+          CustomTextFormField(
+            controller: TextEditingController(),
+            keyboardType: TextInputType.name,
+            labelText: 'お名前',
+            iconData: Icons.short_text,
+          ),
+          const SizedBox(height: 16),
+          CustomTextFormField(
+            controller: TextEditingController(),
+            keyboardType: TextInputType.emailAddress,
+            labelText: 'メールアドレス',
+            iconData: Icons.email,
+          ),
+          const SizedBox(height: 16),
+          RoundLgButton(
+            labelText: '変更内容を保存',
+            labelColor: Colors.white,
+            backgroundColor: Colors.blue,
+            onPressed: () {},
+          ),
+          const SizedBox(height: 16),
+          RoundLgButton(
+            labelText: 'ログアウト',
+            labelColor: Colors.red,
+            borderColor: Colors.red,
+            onPressed: () {},
           ),
         ],
       ),
